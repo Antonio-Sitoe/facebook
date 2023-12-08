7b841ad2588d20c99d0dd90448ded83a479d2c02
Antonio Sitoe <antoniositoehl@gmail.com>
2023-12-08 09:16:34 +0200

    sds

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import IMg from './assets/fb.svg'
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from "react";
+import IMg from "./assets/fb.svg";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+
+  async function handleSubmit() {}
+
+  function handleChangeIn(e: any) {
+    setEmail(e.target.value as string);
+  }
+  function changePassword(e: any) {
+    setpassword(e.target.value);
+  }
   return (
-    <>
-      <div className="container sm:mt-16 lg:mt-48 flex flex-col mx-auto items-center justify-center flex-wrap sm:flex-row">
+    <form onSubmit={handleSubmit}>
+      <div className="container mt-4 sm:mt-16 lg:mt-48 flex flex-col mx-auto items-center justify-center flex-wrap sm:flex-row">
         <div className="left w-1/3 mx-14">
           <img src={IMg} className="w-80" />
           <p className="text-3xl mx-8 hidden sm:block">
@@ -16,13 +28,20 @@ function App() {
             type="text"
             placeholder="Email address or phone number"
             className="px-4 h-12 my-2 border border-1 border-gray-400 rounded-md focus:outline-blue-500"
+            value={email}
+            onChange={handleChangeIn}
           />
           <input
             type="password"
             placeholder="Password"
             className="px-4 h-12 my-2 border border-1 border-gray-400 rounded-md focus:outline-blue-500"
+            value={password}
+            onChange={changePassword}
           />
-          <button className="bg-blue-600 text-white font-bold my-2 py-3 rounded-md text-xl hover:cursor-pointer hover:bg-blue-700">
+          <button
+            type="submit"
+            className="bg-blue-600 text-white font-bold my-2 py-3 rounded-md text-xl hover:cursor-pointer hover:bg-blue-700"
+          >
             Log in
           </button>
           <span className="text-center text-blue-700 text-sm my-2 hover:cursor-pointer hover:underline hover:text-blue-800">
@@ -40,7 +59,7 @@ function App() {
           </span>
         </div>
       </div>
-    </>
+    </form>
   );
 }
 
